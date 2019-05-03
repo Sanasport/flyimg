313e892f2e1c96aec41e783af11333e4f18b9e64
Roman Vohník <vohnik@gmail.com>
2019-05-03 16:15:20 +0200

    Added new route /post for direct image upload as base64 encoded string in POST.

--- src/Core/Controller/DefaultController.php
+++ src/Core/Controller/DefaultController.php
@@ -3,6 +3,7 @@
 namespace Core\Controller;
 
 use Core\Entity\Response;
+use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends CoreController
 {
@@ -42,6 +43,38 @@ class DefaultController extends CoreController
         $image = $this->imageHandler()->processImage($options, $imageSrc);
 
         $this->response->generatePathResponse($image);
+
+        return $this->response;
+    }
+
+    /**
+     * Get image in POST data as base64 encoded string.
+     *
+     * @param string $options
+     *
+     * @return Response
+     * @throws \Exception
+     */
+    public function postAction(string $options): Response
+    {
+        $request = Request::createFromGlobals();
+
+        $encodedImage = $request->get('image');
+
+        if ($encodedImage === null || strlen($encodedImage) === 0) {
+            $this->response->setStatusCode(500, 'Missing "image" in post data');
+        } else {
+            $inputImage = base64_decode($encodedImage);
+
+            $filename = TMP_DIR . uniqid('/post-original-', true);
+            file_put_contents($filename, $inputImage);
+
+            $outputImage = $this->imageHandler()->processImage($options, $filename);
+
+            $this->response->generateImageResponse($outputImage);
+
+            unlink($filename);
+        }
 
         return $this->response;
     }
